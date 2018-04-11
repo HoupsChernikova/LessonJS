ccae51f72d435574bc0e3210a9198d7bd5b619c2
Nadiia <chernikovahoups@gmail.com>
2018-04-11 20:20:35 +0200

    урок 11

--- урок10.1/js/script.js
+++ урок10.1/js/script.js
@@ -1,4 +1,4 @@
-window.addEventListener('DOMContentLoaded', ()  =>{
+window.addEventListener('DOMContentLoaded', () => {
 
     let tab = document.getElementsByClassName('info-header-tab');
     tabContent = document.getElementsByClassName('info-tabcontent');
@@ -98,18 +98,98 @@ window.addEventListener('DOMContentLoaded', ()  =>{
 
 
     for (let i = 0; i < description_btn.length; i++) {
-        
-    description_btn[i].addEventListener('click', () => {
-        description_btn[i].classList.add('more-splash');
-        overlay_LMI.style.display = "block";
-        document.body.style.overflow = 'hidden';
-    });
+
+        description_btn[i].addEventListener('click', (event) => {
+            event.target.classList.add('more-splash');
+            overlay_LMI.style.display = "block";
+            document.body.style.overflow = 'hidden';
+        });
     }
 
-    close_LMI.addEventListener('click', () => {
+    close_LMI.addEventListener('click', (event) => {
         overlay_LMI.style.display = "none";
-        description_btn.classList.remove('more-splash');
+
+        for (let i = 0; i < description_btn.length; i++) {
+            description_btn[i].classList.remove('more-splash');
+        }
+
         document.body.style.overflow = '';
     });
+
+    //form
+    let message = new Object();
+    message.loading = "Загрузка...";
+    message.success = "Спасибо! Скоро мы с вами свяжемся";
+    message.failure = "Что-то пошло не так...";
+
+    let form = document.getElementsByClassName('main-form')[0],
+        input = form.getElementsByTagName('input'),
+        statusMessage = document.createElement('div'),
+        statusMessage.classList.add('status');
+        form.appendChild(statusMessage);
+
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
+        
+        //AJAX
+        let request = new XMLHttpRequest();
+        request.open("POST", 'server.php')
+
+        request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+
+        let formData = new FormData(form);
+        request.send(formData);
+
+        request.onreadystatechange = function() {
+            if (request.readyState < 4) {
+                statusMessage.innerHTML = message.loading;
+            } else if (request.readyState === 4) {
+                if (request.status == 200 && request.status < 300) {
+                    statusMessage.innerHTML = message.success;
+                } else {
+                    statusMessage.innerHTML = message.failure;
+                }
+            }
+        }
+        for (let i = 0; i < input.length; i++) {
+            input[i].value = '';
+        }
+    });
+
+    let formContact = document.getElementById('form'),
+        inputContact = formContact.getElementsByTagName('input'),
+        statusMessageContact = document.createElement('div'),
+        statusMessageContact.classList.add('status');
+        formСontact.appendChild(statusMessageContact);
+
+    formContact.addEventListener('submit', function(event) {
+        event.preventDefault();
+        
+        //AJAX
+        let request = new XMLHttpRequest();
+        request.open("POST", 'server.php')
+
+        request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+
+        let formData = new FormData(form);
+        request.send(formData);
+
+        request.onreadystatechange = function() {
+            if (request.readyState < 4) {
+                statusMessageСontact.innerHTML = message.loading;
+            } else if (request.readyState === 4) {
+                if (request.status == 200 && request.status < 300) {
+                    statusMessageContact.innerHTML = message.success;
+                } else {
+                    statusMessageContact.innerHTML = message.failure;
+                }
+            }
+        }
+        for (let i = 0; i < inputContact.length; i++) {
+            inputContact[i].value = '';
+        }
+    });
+
+
 
 });
