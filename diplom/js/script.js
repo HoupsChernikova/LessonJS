201aca3fd5c312268689bf66e7994e54195f4295
Nadiia <chernikovahoups@gmail.com>
2018-04-24 18:28:57 +0200

    испаравления

--- diplom/js/script.js
+++ diplom/js/script.js
@@ -50,7 +50,6 @@ window.addEventListener('DOMContentLoaded', () => {
 
     });
 
-
     function addNewCundidat() {
         modalWindow.style.display = "none";
         overlay.style.display = "none";
@@ -96,45 +95,36 @@ window.addEventListener('DOMContentLoaded', () => {
         return card;
     }
 
-
-
     ready.addEventListener('click', (event) => {
         if (name.value == '' || biography.value == '' || age.value == '') {
             event.preventDefault();
-            alert('Вы не ввели ничего, есть пустые значения');           
+            alert('Вы не ввели ничего, есть пустые значения');
             return event;
 
         }
-        if (age.value < 18 || age.value > 100 || isNaN(age.value)) {
+        if (age.value < 35 || age.value > 100 || isNaN(age.value)) {
             event.preventDefault();
-            alert('Вы ввели не действительный возраст кандидата.Возраст кандидата не может быть меньше 18 и больше 100');
-           age.value='';
-           
+            alert('Вы ввели не действительный возраст кандидата.Возраст кандидата не может быть меньше 35 и больше 100');
+            age.value = '';
+
             return event;
         }
         let per_name = /[^А-я\s]+/i;
         if (per_name.test(name.value)) {
             event.preventDefault();
             alert('Разрешены только русские буквы');
-             name.value='';           
+            name.value = '';
             return event;
 
         }
-        if (biography.value.length <=10 || !biography.value.replace(/[^А-я\s]+/i, "")) {
+        if (biography.value.length <= 10 || !biography.value.replace(/[^А-я\s]+/i, "")) {
             event.preventDefault();
-            alert('Слишком короткая биография, добавьте информации о кандидате');             
+            alert('Слишком короткая биография, добавьте информации о кандидате');
             return event;
 
         }
-        /*else {
-            name.value.toLowerCase();
-        }*/
-
-
-        addCustom(event);
-        firstRating = 0;
-        secondRating = 0;
-        thirdRating = 0;
+       
+        addCustom(event);        
         setResult("res1", firstRating);
         setResult("res2", secondRating);
         setResult("res3", thirdRating);
@@ -159,12 +149,54 @@ window.addEventListener('DOMContentLoaded', () => {
     function getRandomInt(min, max) {
         return Math.floor(Math.random() * (max - min + 1)) + min;
     }
+   //Voiting result 
+    function setResult(id, value) {
+        let res = document.getElementById(id);
+        let progressBar = res.getElementsByClassName('progress-bar')[0];
+        let resCount = res.getElementsByClassName('result-count')[0];
+        resCount.innerText = value + '%';
+        progressBar.style.height = value + '%';
+        updateBorderCandidate();
+    }
+    // hold an election
+   let honestVoting = document.getElementById("voting");
+
+    honestVoting.addEventListener('click', () => {       
+        random();
+       setResult("res1", firstRating);
+        setResult("res2", secondRating);
+        setResult("res3", thirdRating);       
+
+    });
+
+    let intervene = document.getElementById("crime");
+   
+    function randomAddPlus() {
+        firstRating = getRandomInt(1, 100);
+        secondRating = getRandomInt(1, 100);
+        thirdRating = getRandomInt(1, 100);
+        result = firstRating + secondRating + thirdRating;
+
+        while (result !== 100) {
+            irstRating = getRandomInt(1, 100);
+            secondRating = getRandomInt(1, 100) + 25;
+            thirdRating = getRandomInt(1, 100);
+            result = firstRating + secondRating + thirdRating;
+        }
+    }
+
+    intervene.addEventListener('click', () => {      
+
+       randomAddPlus();
+        setResult("res1", firstRating);
+        setResult("res2", secondRating);
+        setResult("res3", thirdRating);     
+    });
+    
 
     function addCustom(event) {
         custom.style.display = "none";
         index.style.display = "block";
-
-
         let sex = "Неизвестно кто";
         if (genderMale.checked) {
             sex = "Мужчина";
@@ -185,8 +217,6 @@ window.addEventListener('DOMContentLoaded', () => {
             view,
             biography.value
         );
-
-
 
         let existingCard = document.getElementById(MyCardId);
 
@@ -241,84 +271,20 @@ window.addEventListener('DOMContentLoaded', () => {
     }
     // add centre photo
     function photo(currentSlide, isMan) {
-
-
-
         let women = ['person-woman1', 'person-woman2', 'person-woman3', 'person-woman4'],
             men = ['person-man1', 'person-man2', 'person-man3', 'person-man4'];
         let classNames = isMan ? men : women;
         personEasy.className = classNames[currentSlide];
-
-
     }
     // add photoMin in index
     function addPhotoMin(currentSlide, isMan) {
 
         let women = ['photowomen1', 'photowomen2', 'photowomen3', 'photowomen4'],
             men = ['photomen1', 'photomen2', 'photomen3', 'photomen4'];
-
         let classNames = isMan ? men : women;
-
         return classNames[currentSlide];
-
-
     }
-    //Voiting result 
 
-
-    function setResult(id, value) {
-        let res = document.getElementById(id);
-        let progressBar = res.getElementsByClassName('progress-bar')[0];
-        let resCount = res.getElementsByClassName('result-count')[0];
-        resCount.innerText = value + '%';
-        progressBar.style.height = value + '%';
-        updateBorderCandidate();
-    }
-    // hold an election
-    let honestVoting = document.getElementById("voting");
-    let honestVotingExecuted = false;
-
-
-    honestVoting.addEventListener('click', () => {
-        if (honestVotingExecuted) {
-            return;
-        }
-
-        random();
-
-        setResult("res1", firstRating);
-        setResult("res2", secondRating);
-        setResult("res3", thirdRating);
-        honestVotingExecuted = true;
-        interveneExecuted = false;
-
-    });
-
-    let intervene = document.getElementById("crime");
-    let interveneExecuted = false;
-
-    intervene.addEventListener('click', () => {
-        if (interveneExecuted || !honestVotingExecuted) {
-            return;
-        }
-        if (firstRating <= 12 || secondRating <= 11 || thirdRating <= 0) {
-            interveneExecuted = true;
-            alert(' Вы не можете вмешатся в выборы, попробуйте проголосовать честно');
-        } else {
-            firstRating = firstRating - 13;
-            secondRating = secondRating - 12;
-            thirdRating = thirdRating + 25;
-        }
-
-
-
-        setResult("res1", firstRating);
-        setResult("res2", secondRating);
-        setResult("res3", thirdRating);
-        interveneExecuted = true;
-        honestVotingExecuted = false;
-
-    });
     let active1 = document.getElementById('frame1'),
         active2 = document.getElementById('frame2'),
         active3 = document.getElementById(MyCardId);
@@ -344,9 +310,5 @@ window.addEventListener('DOMContentLoaded', () => {
             borderCandidate(MyCardId, true);
         }
     }
-
-
-
-
 
 });
